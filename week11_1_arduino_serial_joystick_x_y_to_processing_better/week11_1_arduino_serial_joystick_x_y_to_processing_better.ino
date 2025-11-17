41a35730fd4d4272731eadc1e5e53339f32909ea
Bryant0618 <changshaobo0618@gmail.com>
2025-11-17 16:43:29 +0800

    week11

---
+++ week11_1_arduino_serial_joystick_x_y_to_processing_better/week11_1_arduino_serial_joystick_x_y_to_processing_better.ino
@@ -0,0 +1,53 @@
+//week11_1_arduino_serial_joystick_x_y_to_processing_better
+void setup() {
+  Serial.begin(9600);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(8,OUTPUT);
+}
+int count=0,totalX=0,totalY=0;//要統計平均的值
+int x0=512,y0=512;
+void loop() {
+  delay(30);//要慢一點不然Processing會接不了
+ int x=analogRead(A2);//把搖桿X接在A2
+ int y=analogRead(A3);//把搖桿Y接在A3
+ if(total<20){//想收集20筆資料
+  totalX+=x;//加總要算平均值
+  totalY+=y;//加總要算平均值
+  count++;
+  x0=totalX/count;//算平均 
+  y0=totalY/count;//算平均
+ }
+ //Serial.write(x/4);
+ //Serial.write(y/4);
+ if(abs(x-x0)<25) x=128;//數值變化太小直接放中間值128
+ else x=(x-x0)/4.4+128;//有大的數值就減掉中間值再除四再加128
+ if(abs(y-y0)<25) x=128;//數值變化太小直接放中間值128
+ else y=(y-y0)/4.4+128;//有大的數值就減掉中間值再除四再加128
+ Serial.write(x);//直接送出去
+ Serial.write(y);//直接送出去
+
+
+ //if(x>900)tone(8,784,100);
+ //if(x<100)tone(8,523,100);
+ //if(y>900)tone(8,659,100);
+ //if(y<100)tone(8,500,100);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
